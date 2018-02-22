c2279ffe7af5c0a1b819d7e05898a1d8834de201
vicanso <vicansocanbico@gmail.com>
2018-02-22 21:53:37 +0800

    chore: 增加docker health check

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,8 @@ RUN cd /app \
   && yarn install --production \
   && yarn cache clean \
   && npm run gen-version
+
 CMD ["node", "--experimental-modules", "/app/app"]
+
+HEALTHCHECK --interval=30s --timeout=3s \
+  CMD node --experimental-modules /app/build/check.mjs || exit 1
---
+++ build/check.mjs
@@ -0,0 +1,19 @@
+import request from 'superagent';
+
+import {port} from '../config';
+
+request
+  .get(`http://127.0.0.1:${port}/ping`)
+  .then(res => {
+    const {status} = res;
+    if (status < 200 || status >= 400) {
+      console.error(res.text);
+      process.exit(1);
+      return;
+    }
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
